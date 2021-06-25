710ba42c16f23757fa53d56a916f59b0a61f9f2c
Hime <49251159+Urikohime@users.noreply.github.com>
2021-06-25 23:42:58 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Projekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(120, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AGAIN?\r\n";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "YES";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(240, 240);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "NO";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(434, 431);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(450, 470);
+            this.MinimumSize = new System.Drawing.Size(450, 470);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Memory";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace Projekt
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        bool selected = false;
+        int count = 0;
+        int left = 8;
+        int firstC;
+        int Firstc;
+        Point xy = new Point(12, 12);
+        Bitmap[] panels = new Bitmap[] {
+            Properties.Resources.one, Properties.Resources.two,
+            Properties.Resources.three, Properties.Resources.four,
+            Properties.Resources.five, Properties.Resources.six,
+            Properties.Resources.seven, Properties.Resources.eight,};
+
+        List<PictureBox> Panels = new List<PictureBox>();
+        int[] set = new int[16] { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8 };
+
+        Image backpanel = Properties.Resources.backpanel;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.MouseClick += new MouseEventHandler(start);
+            label1.Text = "START?";
+        }
+
+        private async void OnPicClick(object sender, EventArgs e)
+        {
+            PictureBox PB = (PictureBox)sender;
+            if (!selected && Panels.IndexOf(PB) != Firstc)
+            {
+                for (int an = 0; an < 10; an++)
+                {
+                    PB.Size = new Size(PB.Size.Width - 10, PB.Size.Height);
+                    PB.Location = new Point(PB.Location.X + 5, PB.Location.Y);
+                    await Task.Delay(1);
+                }
+                PB.Image = SetImg(PB.Name);
+                for (int an = 0; an < 10; an++)
+                {
+                    PB.Size = new Size(PB.Size.Width + 10, PB.Size.Height);
+                    PB.Location = new Point(PB.Location.X - 5, PB.Location.Y);
+                    await Task.Delay(1);
+                }
+                count += 1;
+                if (count == 1)
+                {
+                    firstC = int.Parse(PB.Name);
+                    Firstc = Panels.IndexOf(PB);
+                }
+                if (firstC == int.Parse(PB.Name) && count == 2)
+                {
+                    selected = true;
+                    await Task.Delay(1000);
+                    for (int an = 0; an < 10; an++)
+                    {
+                        PB.Size = new Size(PB.Size.Width - 10, PB.Size.Height - 10);
+                        PB.Location = new Point(PB.Location.X + 5, PB.Location.Y + 5);
+                        Panels[Firstc].Size = new Size(Panels[Firstc].Size.Width - 10, Panels[Firstc].Size.Height - 10);
+                        Panels[Firstc].Location = new Point(Panels[Firstc].Location.X + 5, Panels[Firstc].Location.Y + 5);
+                        await Task.Delay(1);
+                    }
+                    Panels[Panels.IndexOf(PB)].Visible = false;
+                    Panels[Firstc].Visible = false;
+                    PB.Size = new Size(0, 100);
+                    PB.Location = new Point(PB.Location.X - 50, PB.Location.Y - 50);
+                    Panels[Firstc].Size = new Size(0, 100);
+                    Panels[Firstc].Location = new Point(Panels[Firstc].Location.X - 50, Panels[Firstc].Location.Y - 50);
+
+                    count = 0;
+                    left -= 1;
+                    selected = false;
+                    Firstc = -1;
+                }
+                else if (count == 2)
+                {
+                    selected = true;
+                    await Task.Delay(1000);
+                    count = 0;
+                    for (int an = 0; an < 10; an++)
+                    {
+                        PB.Size = new Size(PB.Size.Width - 10, PB.Size.Height);
+                        PB.Location = new Point(PB.Location.X + 5, PB.Location.Y);
+                        Panels[Firstc].Size = new Size(Panels[Firstc].Size.Width - 10, Panels[Firstc].Size.Height);
+                        Panels[Firstc].Location = new Point(Panels[Firstc].Location.X + 5, Panels[Firstc].Location.Y);
+                        await Task.Delay(1);
+                    }
+                    PB.Image = backpanel;
+                    Panels[Firstc].Image = backpanel;
+                    for (int an = 0; an < 10; an++)
+                    {
+                        PB.Size = new Size(PB.Size.Width + 10, PB.Size.Height);
+                        PB.Location = new Point(PB.Location.X - 5, PB.Location.Y);
+                        Panels[Firstc].Size = new Size(Panels[Firstc].Size.Width + 10, Panels[Firstc].Size.Height);
+                        Panels[Firstc].Location = new Point(Panels[Firstc].Location.X - 5, Panels[Firstc].Location.Y);
+                        await Task.Delay(1);
+                    }
+                    Firstc = -1;
+                    selected = false;
+                }
+                if (left == 0)
+                {
+                    PictureBox sho = new PictureBox();
+                    sho.ClientSize = new Size(100, 100);
+                    sho.SizeMode = PictureBoxSizeMode.StretchImage;
+                    sho.Location = new Point((this.Size.Width  / 2) - 50, (this.Size.Height / 2) - 50);
+                    PB.Image = backpanel;
+                    this.Controls.Add(sho);
+                    sho.Visible = true;
+                    foreach (Bitmap Imm in panels)
+                    {
+                        for (int an = 0; an < 10; an++)
+                        {
+                            sho.Size = new Size(sho.Size.Width - 10, sho.Size.Height);
+                            sho.Location = new Point(sho.Location.X + 5, sho.Location.Y);
+                            await Task.Delay(1);
+                        }
+                        sho.Image = Imm;
+                        for (int an = 0; an < 10; an++)
+                        {
+                            sho.Size = new Size(sho.Size.Width + 10, sho.Size.Height);
+                            sho.Location = new Point(sho.Location.X - 5, sho.Location.Y);
+                            await Task.Delay(1);
+                        }
+                        await Task.Delay(150);
+                        for (int an = 0; an < 10; an++)
+                        {
+                            sho.Size = new Size(sho.Size.Width - 10, sho.Size.Height);
+                            sho.Location = new Point(sho.Location.X + 5, sho.Location.Y);
+                            await Task.Delay(1);
+                        }
+                        sho.Image = backpanel;
+                        for (int an = 0; an < 10; an++)
+                        {
+                            sho.Size = new Size(sho.Size.Width + 10, sho.Size.Height);
+                            sho.Location = new Point(sho.Location.X - 5, sho.Location.Y);
+                            await Task.Delay(1);
+                        }
+                        await Task.Delay(150);
+                    }
+                    for (int an = 0; an < 10; an++)
+                    {
+                        sho.Size = new Size(sho.Size.Width - 10, sho.Size.Height);
+                        sho.Location = new Point(sho.Location.X + 5, sho.Location.Y);
+                        await Task.Delay(1);
+                    }
+                    sho.Visible = false;
+                    label1.Visible = true;
+                    button1.Visible = true;
+                    button2.Visible = true;
+                }
+            }
+        }
+
+        private async void start(object sender, EventArgs e)
+        {
+            button1.MouseClick += new MouseEventHandler(button1_Click);
+            count = 0;
+            left = 8;
+            firstC = 20;
+            Firstc = 20;
+            label1.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            for (int i = 0; i < 16; i++)
+            {
+                Panels.Add(new PictureBox());
+                Panels[i].Name = SetInd().ToString();
+                Panels[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                Panels[i].Image = backpanel;
+                Panels[i].ClientSize = new Size(0, 100);
+                Panels[i].Location = xy;
+                xy.X += 103;
+                if (xy.X >= 330)
+                {
+                    xy.X = 12;
+                    xy.Y += 103;
+                }
+                Panels[i].Visible = true;
+                Panels[i].MouseClick += new MouseEventHandler(OnPicClick);
+            }
+            foreach (PictureBox PB in Panels)
+            {
+                this.Controls.Add(PB);
+                for (int an = 0; an < 10; an++)
+                {
+                    PB.Size = new Size(PB.Size.Width + 10, PB.Size.Height);
+                    PB.Location = new Point(PB.Location.X, PB.Location.Y);
+                    PB.BackColor = Color.Transparent;
+                    await Task.Delay(1);
+                }
+            }
+        }
+
+        private int SetInd()
+        {
+            int get = r.Next(0, set.Length);
+            int returner = set[get];
+            do
+            {
+                get = r.Next(0, set.Length);
+                returner = set[get];
+            } while (returner == 0);
+            set[get] = 0;
+            return returner;
+        }
+
+        private Bitmap SetImg(String name)
+        {
+            Bitmap ret = null;
+            switch(name)
+            {
+                case "1":
+                    ret = panels[0];
+                    break;
+                case "2":
+                    ret = panels[5];
+                    break;
+                case "3":
+                    ret = panels[4];
+                    break;
+                case "4":
+                    ret = panels[2];
+                    break;
+                case "5":
+                    ret = panels[7];
+                    break;
+                case "6":
+                    ret = panels[1];
+                    break;
+                case "7":
+                    ret = panels[3];
+                    break;
+                case "8":
+                    ret = panels[6];
+                    break;
+            }
+            return ret;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            left = 8;
+            firstC = 20;
+            Firstc = 20;
+            label1.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            set = new int[16] { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8 };
+            foreach (PictureBox PB in Panels)
+            {
+                PB.Image = backpanel;
+                PB.Name = SetInd().ToString();
+                PB.Visible = true;
+                for (int an = 0; an < 10; an++)
+                {
+                    PB.Size = new Size(PB.Size.Width + 10, PB.Size.Height);
+                    PB.Location = new Point(PB.Location.X, PB.Location.Y);
+                    PB.BackColor = Color.Transparent;
+                    await Task.Delay(1);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
